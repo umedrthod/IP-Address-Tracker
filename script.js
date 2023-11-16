d1e990b0b53d9afbf61f0ed68ab4978198bbb8e8
umedrthod <86955568+umedrthod@users.noreply.github.com>
2023-11-16 23:12:37 +0530

    Refactor

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 "use strict";
-const form = document.querySelector(".search-form");
-const input = document.querySelector(".search-input");
-const result = document.querySelector("#result");
+
+// DOM elements
+const form = document.querySelector("#searchForm");
+const input = document.querySelector("#ipAddressInput");
+const result = {
+    ipAddress: document.querySelector("#ipAddress"),
+    location: document.querySelector("#location"),
+    timezone: document.querySelector("#timezone"),
+    isp: document.querySelector("#isp")
+};
 const mapContainer = document.querySelector("#map");
 let map;
 
-map = L.map("map").setView([0,0], 13);
+// Initialize Leaflet map
+map = L.map("map").setView([0, 0], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Event listener for form submission
 form.addEventListener("submit", handleSubmit);
 
+/**
+* Handles the submit event of the form
+* @param {Event} event - the submit event
+* @returns {Promise} - a promise that resolves with the IP address data
+*/
 async function handleSubmit(event) {
     event.preventDefault();
     const ipAddress = input.value;
@@ -21,7 +35,7 @@ async function handleSubmit(event) {
 
     if (checkIpAddress.test(ipAddress) || checkDomain.test(ipAddress)) {
         try {
-            const apiKey = "444ff91e67094375870b72698cb5ba58"
+            const apiKey = "444ff91e67094375870b72698cb5ba58";
             const res = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ipAddress}`);
             const data = await res.json();
             setAddress(data);
@@ -31,35 +45,20 @@ async function handleSubmit(event) {
     }
 }
 
+/**
+* Sets the address information on the page
+* @param {Object} address - the address object
+*/
 function setAddress(address) {
     if (address) {
-        result.innerHTML = `
-        <div class="result-container">
-            <article>
-            <h2>IP Address</h2>
-            <p>${address.ip}</p>
-            </article>
-
-            <article>
-            <h2>Location</h2>
-            <p>${address.city}, ${address.state_prov}</p>
-            </article>
-
-            <article>
-            <h2>Timezone</h2>
-            <p>${address.time_zone.name}</p>
-            </article>
-
-            <article>
-            <h2>ISP</h2>
-            <p>${address.isp}</p>
-            </article>
-        </div>
-        `;
+        result.ipAddress.textContent = address.ip || "";
+        result.location.textContent = `${address.city}, ${address.state_prov}` || "";
+        result.timezone.textContent = address.time_zone.name || "";
+        result.isp.textContent = address.isp || "";
 
         const lat = address.latitude || 0;
-        const lng = address.longitude ||0;
-        
+        const lng = address.longitude || 0;
+
         map.setView([lat, lng], 13);
         L.marker([lat, lng]).addTo(map);
     }
